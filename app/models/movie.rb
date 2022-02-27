773c177b40f19924a5f9f2b5c6f4728e0f1cf802
Ubuntu <ubuntu@ip-172-31-4-104.us-east-2.compute.internal>
2022-02-27 23:35:42 +0000

    part 2 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ActiveRecord::Base
     def self.all_ratings
-        %w(G PG PG-13 NC-17 R)
+        Movie.select(:rating).distinct.inject([]) { |a, m| a.push m.rating }
     end
 end
